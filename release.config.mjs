38ac4020c748b3008f50c5a150bf5aba87a6b065
Marc Gavanier <marc@gavanier.com>
2026-04-20 01:12:26 +0200

    chore: initialize project tooling

---
+++ release.config.mjs
@@ -0,0 +1,27 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+    '@semantic-release/npm',
+    ['@semantic-release/exec', { prepareCmd: 'pnpm biome format --write package.json' }],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        // biome-ignore lint/suspicious/noTemplateCurlyInString: semantic-release template syntax
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    '@semantic-release/github'
+  ]
+};
